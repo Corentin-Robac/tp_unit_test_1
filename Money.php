ce1c1dbe3e0be360ac323654da2d38ed0d4e4853
Corentin Robac <corentin.robac@laposte.net>
2024-12-16 14:59:51 +0100

    Initial Commit

---
+++ Money.php
@@ -0,0 +1,19 @@
+<?php
+class Vegetable {
+
+    // Datas
+    public $data1;
+
+    // Constructor
+    public function __construct( $data1 = "green")
+    {
+        $this->elementData = $data1;
+    }
+
+    // Methods
+    public function getData()
+    {
+        return $this->data;
+    }
+}
+?>
